7f8f98ae97e9e4f97fca165efb94ea3eead4e5eb
minlinea <min_linea@naver.com>
2022-01-20 09:34:01 +0900

    서버 Listen 스레드 추가 및 버그 수정
    서버 Listen 스레드 추가,

--- GDRintern_Server/CServer.cpp
+++ GDRintern_Server/CServer.cpp
@@ -8,10 +8,11 @@ CServer::CServer()
 
 CServer::~CServer()
 {
-	closesocket(this->m_hListen);
-	CloseHandle(this->m_hSend);
-	CloseHandle(this->m_hRecv);
-	//closehandle(this->m_hMutex);
+	closesocket(Instance().m_hListenSock);
+	CloseHandle(Instance().m_hSend);
+	CloseHandle(Instance().m_hRecv);
+	CloseHandle(Instance().m_hListen);
+	//CloseHandle(Instance().m_hMutex);
 	WSACleanup();
 }
 
@@ -19,25 +20,54 @@ void CServer::ServerInit()
 {
 	WSAStartup(MAKEWORD(2, 2), &this->m_wsaData);
 
-	this->m_hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	Instance().m_hListenSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	this->m_tListenAddr.sin_family = AF_INET;
-	this->m_tListenAddr.sin_port = htons(PORT);
-	this->m_tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	Instance().m_tListenAddr.sin_family = AF_INET;
+	Instance().m_tListenAddr.sin_port = htons(PORT);
+	Instance().m_tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	bind(this->m_hListen, (SOCKADDR*)&this->m_tListenAddr, sizeof(this->m_tListenAddr));
+	bind(Instance().m_hListenSock, (SOCKADDR*)&Instance().m_tListenAddr, sizeof(Instance().m_tListenAddr));
 
-	listen(this->m_hListen, SOMAXCONN);
-	std::cout << "serverInit\n";
+	listen(Instance().m_hListenSock, SOMAXCONN);
+}
+
+void CServer::err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER
+		| FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
 }
 
 DWORD WINAPI CServer::ListenThread(LPVOID socket)
 {
+	SOCKET t = Instance().m_hListenSock;
+	std::cout << "Server Listen\n";
+	while (true)
+	{
+		SOCKADDR_IN tCIntAddr = {};
+		int iCIntSize = sizeof(tCIntAddr);
+		Instance().m_hClient = accept(Instance().m_hListenSock, (SOCKADDR*)&tCIntAddr, &iCIntSize);
+		std::cout << "[login]Client IP : " << inet_ntoa(tCIntAddr.sin_addr) << std::endl;
+
+		DWORD dwSendThreadID, dwRecvThreadID;
+		Instance().m_hRecv = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)CServer::Instance().RecvThread, (LPVOID)Instance().m_hClient, 0, &dwRecvThreadID);
+		Instance().m_hSend = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)CServer::Instance().SendThread, (LPVOID)Instance().m_hClient, 0, &dwSendThreadID);
+
+		DWORD retvalSend = WaitForSingleObject(Instance().m_hSend, INFINITE);
+		std::cout << "[logout]Client IP : " << inet_ntoa(tCIntAddr.sin_addr) << std::endl;
+		closesocket(Instance().m_hClient);
+	}
 	return NULL;
 }
 
-
- DWORD WINAPI CServer::SendThread(LPVOID socket)
+DWORD WINAPI CServer::SendThread(LPVOID socket)
 {
 	while (true)
 	{
@@ -89,29 +119,9 @@ DWORD WINAPI CServer::RecvThread(LPVOID socket)
 
 void CServer::ServerAccept()
 {
-	while (true)
-	{
-		std::cout << "ServerAccept\n";
-		SOCKADDR_IN tCIntAddr = {};
-		int iCIntSize = sizeof(tCIntAddr);
-		this->m_hClient = accept(this->m_hListen, (SOCKADDR*)&tCIntAddr, &iCIntSize);		//클라이언트가 죽어도 얘는 통신 대기.
-		std::cout << "[login]Client IP : " << inet_ntoa(tCIntAddr.sin_addr) << std::endl;
-
-		DWORD dwSendThreadID, dwRecvThreadID;
-
-	
-
-		this->m_hSend = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)CServer::SendThread, (LPVOID)this->m_hClient, 0, &dwSendThreadID);
-
-		this->m_hRecv = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)CServer::RecvThread, (LPVOID)this->m_hClient, 0, &dwRecvThreadID);
-
-		std::thread t(&CServer::SendThread, (LPVOID)this->m_hClient);
-
-		DWORD retvalSend = WaitForSingleObject(this->m_hSend, INFINITE);
-		std::cout << "[logout]Client IP : " << inet_ntoa(tCIntAddr.sin_addr) << std::endl;
-		closesocket(this->m_hClient);
-	}
-
+	DWORD dwListenThreadID;
+	this->m_hListen = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)CServer::ListenThread,
+										(LPVOID)Instance().m_hClient, 0, &dwListenThreadID);
 }
 
 int CServer::Server_Send(const SOCKET& sock, const void* buf, int len)
