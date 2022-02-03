1fa5ec2d0cb4fe8187332b209c529a9dd9202be6
minlinea <min_linea@naver.com>
2022-02-03 16:50:52 +0900

    Log 이해하기 3 - 원하는 경로 설정
    - log파일 저장 시 실행한 해당 프로젝트 안에 log폴더 안에 log를 작성하라고 하고 싶어 경로 파일 얻는 부분 이용 함수 수정

--- GDRintern/CLog.cpp
+++ GDRintern/CLog.cpp
@@ -2,22 +2,16 @@
 
 CLog::CLog()
 {
+	::CreateDirectoryA("Log", NULL);				//작업중인 해당 프로젝트
 	SetPathFile();
 }
 
 void CLog::SetPathFile()
 {
 	char caPath[MAX_PATH] = { 0, };
-	GetModuleFileNameA(NULL, caPath, MAX_PATH);
+	GetCurrentDirectoryA(MAX_PATH, caPath);			//작업중인 해당 프로젝트
 
-	char szDrive[_MAX_DRIVE] = { 0, };
-	char szDir[MAX_PATHNAME_LEN] = { 0, };
-	char szName[_MAX_FNAME] = { 0, };
-	char szExt[_MAX_EXT] = { 0, };
-	_splitpath_s(caPath, szDrive, _MAX_DRIVE, szDir, MAX_PATHNAME_LEN, szName, _MAX_FNAME, szExt, _MAX_EXT);
-	//szName(실행프로그램 이름) + szExt(실행프로그램 타입) 
-
-	sprintf_s(m_caPathName, MAX_PATHNAME_LEN, "%s%s", szDrive, szDir);
+	sprintf_s(m_caPathName, MAX_PATHNAME_LEN, "%s%s", caPath, "\\log\\");	//그곳에 생성된 log폴더
 	sprintf_s(m_caFileName, MAX_FILENAME_LEN, "%s", SAVEFILENAME);
 }
 
@@ -27,14 +21,14 @@ void CLog::Log(const char * loglevel, const char* logmsg)
 	GetLocalTime(&t);
 
 	FILE* pFile = NULL;
-	char szFileName[MAX_PATHNAME_LEN] = { 0, };
+	char szFileName[MAX_PATHNAME_LEN] = { 0, };		//생성할 파일 경로 + 이름
 	sprintf_s(szFileName, "%s%s_%04d%02d%02d.log", m_caPathName, m_caFileName, t.wYear, t.wMonth, t.wDay);
 
-	fopen_s(&pFile, szFileName, "at");
+	fopen_s(&pFile, szFileName, "at");		//append text 모드
 	if (pFile)
 	{
-		fprintf_s(pFile, "[%02d:%02d:%02d.%03d][%s] %s\n", t.wHour, t.wMinute, t.wSecond, t.wMilliseconds, loglevel, logmsg);	//마지막 test를 인자로 받아 사용
-
+		fprintf_s(pFile, "[%02d:%02d:%02d.%03d][%s] %s\n", t.wHour, t.wMinute, t.wSecond, t.wMilliseconds, loglevel, logmsg);
+									//로그 내용 작성
 		fclose(pFile);
 	}
 	else
--- GDRintern/소스.cpp
+++ GDRintern/소스.cpp
@@ -8,12 +8,9 @@
 #include "CLog.h"
 using namespace std;
 
-#define MAX_PATHNAME_LEN 1024
-#define MAX_FILENAME_LEN 256
-#define MAX_MESSAGE_LEN 256
-
 int main()
 {
+	CLog clog;
 	char c;
 	while(true)
 	{
@@ -32,98 +29,11 @@ int main()
 			}
 			else if (c == 'w')
 			{
-				char path[MAX_PATH] = { 0, };
-				GetModuleFileNameA(NULL, path, MAX_PATH);
-				string str(path);
-				int t = str.rfind('\\');		//마지막 \있는 지점을 찾아낸 위치
-				string str2 = str.substr(t, str.size() - t);	//해당 파일의 이름을 얻어냄 (\+[실행 프로그램 파일])
-				str.erase(str.rfind('\\'));						//해당 부분만 지워낼 수도 있음
-				cout << str2 << " " << str << endl;
+				clog.Log("error", "에러러러러러러러러");
+				cout << "write" << endl;
+
 			}
-			else if (c == 'e')
-			{
-				char path[MAX_PATH] = { 0, };
-				char pathname[MAX_PATH] = { 0, };
-				char filename[MAX_PATH] = { 0, };
-				char test[MAX_PATH] = { 0, };
-				GetModuleFileNameA(NULL, path, MAX_PATH);
-
-				char szDrive[_MAX_DRIVE] = { 0, };
-				char szDir[MAX_PATHNAME_LEN] = { 0, };
-				char szName[_MAX_FNAME] = { 0, };
-				char szExt[_MAX_EXT] = { 0, };
-				_splitpath_s(path, szDrive, _MAX_DRIVE, szDir, MAX_PATHNAME_LEN, szName, _MAX_FNAME, szExt, _MAX_EXT);
-
-				sprintf_s(pathname, MAX_PATHNAME_LEN, "%s%s", szDrive, szDir);
-				sprintf_s(filename, MAX_FILENAME_LEN, "%s", "TEST.txt");
-				sprintf_s(test, MAX_FILENAME_LEN, "%s%s", szName, szExt);
-
-				string drive(szDrive);	//Drive만 (C, D, E 이런거)
-				string dir(szDir);		//Drive를 제외한 경로(\~)
-				string str(pathname);	//sprintf_s로 조합한 파일 경로
-				string str2(filename);	//sprintf_s로 설정한 파일 이름
-				string t(szName);		//해당 프로그램의 이름
-				string t2(szExt);		//해당 프로그램의 실행 타입
-				string t3(test);		//sprintf_s로 조합한 이름 + 실행 타입
-
-				cout << drive << endl;	//C:
-				cout << dir << endl;	//\Users\line0413\Desktop\인턴십\GDR인턴프로젝트\GDRintern\Debug\//
-				cout << str << endl;	//C:\Users\line0413\Desktop\인턴십\GDR인턴프로젝트\GDRintern\Debug\//
-				cout << str2 << endl;	//TEST.txt
-				cout << t << endl;		//GDRintern
-				cout << t2 << endl;		//.exe
-				cout << t3 << endl;		//GDRintern.exe					
-			}
-			else if (c == 'r')
-			{
-				SYSTEMTIME t;
-				GetLocalTime(&t);
-				int date[6] = { t.wYear, t.wMonth, t.wDay, t.wHour, t.wMinute, t.wSecond};
-				cout << "[" << date[0] << "." << date[1] << "." << date[2] << " " << date[3] << ":"
-					<< date[4] << ":" << date[5] << "] : " << endl;
-			}
-			else if (c == 't')
-			{
-				char path[MAX_PATH] = { 0, };
-				char pathname[MAX_PATH] = { 0, };
-				char filename[MAX_PATH] = { 0, };
-				GetModuleFileNameA(NULL, path, MAX_PATH);
-
-				char szDrive[_MAX_DRIVE] = { 0, };
-				char szDir[MAX_PATHNAME_LEN] = { 0, };
-				char szName[_MAX_FNAME] = { 0, };
-				char szExt[_MAX_EXT] = { 0, };
-				_splitpath_s(path, szDrive, _MAX_DRIVE, szDir, MAX_PATHNAME_LEN, szName, _MAX_FNAME, szExt, _MAX_EXT);
-
-				sprintf_s(pathname, MAX_PATHNAME_LEN, "%s%s", szDrive, szDir);
-				sprintf_s(filename, MAX_FILENAME_LEN, "%s", "TEST");
-
-				string str(pathname);	//sprintf_s로 조합한 파일 경로
-				string str2(filename);	//sprintf_s로 설정한 파일 이름
-
-
-				SYSTEMTIME t;
-				GetLocalTime(&t);
-
-				FILE* pFile = NULL;
-				char szFileName[MAX_PATHNAME_LEN] = { 0, };
-				sprintf_s(szFileName, "%s%s_%04d%02d%02d.log", pathname, filename, t.wYear, t.wMonth, t.wDay);
-
-				fopen_s(&pFile, szFileName, "at");
-				if (pFile)
-				{
-					fprintf_s(pFile, "[%02d:%02d:%02d.%03d][%s] %s\n", t.wHour, t.wMinute, t.wSecond, t.wMilliseconds, "Warn", "test");	//마지막 test를 인자로 받아 사용
-
-					cout << "pFile open\n";
-					fclose(pFile);
-				}
-				else
-				{
-					cout << "pFile not open\n";
-				}
-
-				cout << "pFile\n";
-			}
+			
 		}
 		else
 		{
